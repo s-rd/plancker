8d36a802df905da8bfa96fbb98b1d10fb79e11eb
Simon Dagfinrud <17279882+semonje@users.noreply.github.com>
2019-04-11 22:12:28 +0200

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <h1>Configure your keyboard</h1>
+    <main class="keyboard">
+      <ul class="keyboard__keys">
+        <li v-for="key in keys" class="keyboard__key key">
+          <span class="key__title">{{ key.name }}</span>
+          <span class="key__desc">{{ key.code }}</span>
+        </li>
+      </ul>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      keys: [],
+      rowAmount: 4,
+    }
+  },
+  mounted() {
+    for (let i = 0; i < 40; i++) {
+      this.keys.push({
+        name: 'A',
+        code: 'KC_A',
+      })
+    }
+  },
+  computed: {
+    rows() {
+      return []
+    },
+  },
+}
+</script>
