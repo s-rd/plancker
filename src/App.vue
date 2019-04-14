5eb475054dd9d9efa072da690c453aac350430b4
Simon Dagfinrud <17279882+semonje@users.noreply.github.com>
2019-04-14 04:14:15 +0200

    Add menubar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
+    <menu-bar/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import MenuBar from '@/components/MenuBar'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    MenuBar,
+  },
 }
 </script>
---
+++ src/components/MenuBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="nav">
+    <router-link :to="{ name: 'home' }" class="nav__logo">Plancker</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'menu-bar',
+}
+</script>
