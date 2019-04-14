9bbed73ecbc7247e03759379d827f5705d116626
Simon Dagfinrud <17279882+semonje@users.noreply.github.com>
2019-04-14 04:06:13 +0200

    Lots of scribbling around

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,32 +1,37 @@
 <template>
   <div class="home">
-    <h1>Configure your keyboard</h1>
-    <main class="keyboard">
-      <ul class="keyboard__keys">
-        <li v-for="key in keys" class="keyboard__key key">
-          <span class="key__title">{{ key.name }}</span>
-          <span class="key__desc">{{ key.code }}</span>
-        </li>
-      </ul>
+    <main class="keyboard__list">
+      <div class="keyboard keyboard--ghost keyboard--ghost-upper">
+        <ul class="keyboard__keys">
+          <li v-for="key in 48" class="keyboard__key key"></li>
+        </ul>
+      </div>
+      <div class="keyboard">
+        <ul class="keyboard__keys">
+          <li v-for="key in keys" class="keyboard__key key">
+            <span class="key__name">{{ key.name }}</span>
+            <span class="key__code">{{ key.code }}</span>
+          </li>
+        </ul>
+      </div>
+      <div class="keyboard keyboard--ghost keyboard--ghost-lower">
+        <ul class="keyboard__keys">
+          <li v-for="key in 48" class="keyboard__key key"></li>
+        </ul>
+      </div>
     </main>
   </div>
 </template>
 
 <script>
+import keymap from '@/assets/keymaps/semonje'
+
 export default {
   name: 'home',
   data () {
     return {
-      keys: [],
+      keys: keymap,
       rowAmount: 4,
-    }
-  },
-  mounted() {
-    for (let i = 0; i < 40; i++) {
-      this.keys.push({
-        name: 'A',
-        code: 'KC_A',
-      })
     }
   },
   computed: {
